2afe16ab464479ad4a16acf7ae01b91dde3cb624
mumble <andreapauline.deguzman@gmail.com>
2024-04-15 05:05:02 +0800

    JSON FILE LOAD - printing all data in json

--- pyvis-test.py
+++ pyvis-test.py
@@ -2,3 +2,12 @@ from pyvis.network import Network
 import json
 
 file = "./json_files/alcuin_letters.json"
+
+def get_data(): 
+  with open (file, "r") as json_file: 
+    data = json.load(json_file)
+    return(data)
+  
+epp_data = get_data()
+
+print(epp_data)
